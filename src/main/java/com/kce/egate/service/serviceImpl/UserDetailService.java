bcfcb1d06738caa4b0802b6ff498a4f65c2204f9
deepu144 <deepaktmalai65@gmil.com>
2024-09-01 17:05:03 +0530

    feat: bug fixes

--- src/main/java/com/kce/egate/service/serviceImpl/UserDetailService.java
+++ src/main/java/com/kce/egate/service/serviceImpl/UserDetailService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserDetailService implements UserDetailsService {
     private final UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmail(username).orElseThrow();
