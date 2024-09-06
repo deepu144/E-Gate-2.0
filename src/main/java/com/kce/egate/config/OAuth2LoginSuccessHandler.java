a7947320c9ee4f838e7dd9c80a590ded9038c38f
deepu144 <deepaktmalai65@gmil.com>
2024-09-06 21:38:35 +0530

    feat: testing

--- src/main/java/com/kce/egate/config/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/kce/egate/config/OAuth2LoginSuccessHandler.java
@@ -65,6 +65,7 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
         String jsonResponse = objectMapper.writeValueAsString(commonResponse);
         String encodedResponse = Base64.getEncoder().encodeToString(jsonResponse.getBytes(StandardCharsets.UTF_8));
         LOGGER.info("** loginSuccess {}",encodedResponse);
+        LOGGER.info("** loginSuccess {}",commonResponse);
         String redirectUrl = "http://localhost:3000/auth/oauth2/callback"
                 + "?data=" + URLEncoder.encode(encodedResponse, StandardCharsets.UTF_8);
         getRedirectStrategy().sendRedirect(request, response, redirectUrl);
